737f49e6a60cb4efe25d42c94c6c91f97d156c07
Chris-dagostino <dagostino.christian94@gmail.com>
2022-02-09 15:07:59 +0100

    start vue

---
+++ main.js
@@ -0,0 +1,13 @@
+//!Descrizione: Stampare a schermo un messaggio all’interno di un h1, 
+//!utilizzando i data. 
+
+const app = new Vue (
+    el: '#root',
+    data: {
+        message: 'vedi di imparare ad utilizzare Vue!', 
+    }
+);
+
+
+
+//!Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
